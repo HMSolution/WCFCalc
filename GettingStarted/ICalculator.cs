16d809e3606869c3395fb4f4e936fe4a8510f280
HMSolution <leon.rengshausen@hauertmann.net>
2019-11-12 17:28:42 +0100

    Added solution files

---
+++ GettingStarted/ICalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace GettingStarted
+{
+    // Definition eines service contracts => Spezifizierung welche Operationen unterstützt werden
+    [ServiceContract (Namespace = "http://Microsoft.ServiceModel.Samples")]
+    public interface ICalculator
+    {
+        //OperationConract dieses Attribut muss über zugängliche Methoden geschrieben werden
+        [OperationContract]
+        double Add(double n1, double n2);
+        [OperationContract]
+        double Subtract(double n1, double n2);
+        [OperationContract]
+        double Multiply(double n1, double n2);
+        [OperationContract] // Es wird keine Antwort an den Client gesendet, eigentlich für Voids vorgesehen, um clients nicht mit leeren Nachrichten zu freezen
+        double Divide(double n1, double n2);
+    }
+}
---
+++ gettingstartedhost/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using GettingStarted;
+using GettingStarted;
+
+namespace GettingStartedHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Step 1: Create a URI to serve as the base address.
+            Uri baseAddress = new Uri("http://localhost:8000/GettingStarted/");
+
+            // Step 2: Create a ServiceHost instance.
+            //Used to host the Service, takes the service and base address
+            ServiceHost selfHost = new ServiceHost(typeof(CalculatorService), baseAddress);
+
+            try
+            {
+                // Step 3: Add a service endpoint.
+                // This is created/composed by a binding, an address and a service contract
+                // The endpoint address is the base address with the service name appended (http://localhost:8000/GettingStarted/CalculatorService) 
+                selfHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "CalculatorService");
+
+                // Step 4: Enable metadata exchange.
+                // Used for creating proxies for calling service operations
+                // Here a "ServiceMetadataBehavior" is created and its "HttpGetEnabled" property is set to true and then added
+                // to the behaviors collection, for enabling metadata exchange
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                // Step 5: Start the service.
+                selfHost.Open();
+                Console.WriteLine("The service is ready.");
+
+                // Close the ServiceHost to stop the service.
+                Console.WriteLine("Press <Enter> to terminate the service.");
+                Console.WriteLine();
+                Console.ReadLine();
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
